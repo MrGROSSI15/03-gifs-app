a51c93848db2a33b68364f1678a3f48307301ec2
Marc Grossi <tecnogrossi@gmail.com>
2024-05-06 23:59:01 +0200

    beginning class 96

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+// const GIPHY_API_KEY = 'nPt6dkw8es2Vw9VzWsiJEPOnXgIt8lwp';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  private _tagsHistory: string[] = [];
+ private apiKey: string = 'nPt6dkw8es2Vw9VzWsiJEPOnXgIt8lwp';
+ private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient) { }
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory (tag: string){
+    tag = tag.toLowerCase();
+
+    if (this.tagsHistory.includes(tag)){
+      this._tagsHistory = this.tagsHistory.filter ( (oldTag) => oldTag !== tag)
+    }
+
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+  }
+
+  searchTag( tag: string) :void {
+    if( tag.length === 0) return;
+    this.organizeHistory(tag)
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search`, {params})
+    .subscribe ( resp =>{
+       console.log(resp);
+    });
+
+
+  }
+
+
+}
