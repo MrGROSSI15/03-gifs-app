96e530bd573a5cfdbb5346be94a4cb22c434f939
Marc Grossi <tecnogrossi@gmail.com>
2024-05-08 19:43:38 +0200

    untill class 110

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,10 @@ private _tagsHistory: string[] = [];
  private apiKey: string = 'nPt6dkw8es2Vw9VzWsiJEPOnXgIt8lwp';
  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+   }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -28,6 +31,19 @@ private _tagsHistory: string[] = [];
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if ( !localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse (localStorage.getItem('history')! );
+
+    if( this._tagsHistory.length === 0) return;
+    this.searchTag( this._tagsHistory[0]);
   }
 
   searchTag( tag: string) :void {
